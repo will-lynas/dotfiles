1edae7f807bfb1d805206ab53faab4bbfdcbb924
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-08-18 17:12:32 +0100

    Unify tmux and nvim pane navigation

--- nvim/lua/lynasw/packer.lua
+++ nvim/lua/lynasw/packer.lua
@@ -33,5 +33,19 @@ return require('packer').startup(function(use)
 		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
 		  {'L3MON4D3/LuaSnip'},     -- Required
 	  }
+  }
+  use { 'alexghergh/nvim-tmux-navigation', config = function()
+      require'nvim-tmux-navigation'.setup {
+          disable_when_zoomed = true, -- defaults to false
+          keybindings = {
+              left = "<C-h>",
+              down = "<C-j>",
+              up = "<C-k>",
+              right = "<C-l>",
+              last_active = "<C-\\>",
+              next = "<C-Space>",
+          }
+      }
+  end
 }
 end)
