5627e601fe1d7e1121e5ede5802731fc4a1dc662
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-08-24 16:43:16 +0100

    Add smartcase and fzf

--- nvim/lua/lynasw/set.lua
+++ nvim/lua/lynasw/set.lua
@@ -18,6 +18,9 @@ vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
