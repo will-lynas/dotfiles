2b9c104a5469bae209d716149f14589ec8491e48
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-08-24 18:28:12 +0100

    Update fzf options

--- bash/fzf_options.sh
+++ bash/fzf_options.sh
@@ -2,4 +2,10 @@ export FZF_DEFAULT_OPTS=\
 '--multi '\
 '--height 40% '\
 '--reverse '\
-'--preview "batcat --style=numbers --color=always --line-range :500 {} 2>/dev/null"'
+'--preview "batcat --style=numbers --color=always --line-range :500 {} 2>/dev/null" '\
+'--border=none '\
+'--info=inline-right '\
+'--no-scrollbar '\
+'--prompt="* " '\
+'--pointer="* " '\
+'--marker="*" '\
