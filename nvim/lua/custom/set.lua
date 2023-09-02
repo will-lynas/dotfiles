8bff438b96978aba933835ce36d3360fc526c250
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-02 13:28:33 +0100

    Enable line wrapping

--- nvim/lua/custom/set.lua
+++ nvim/lua/custom/set.lua
@@ -8,7 +8,8 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -29,7 +30,7 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 
 vim.g.netrw_banner = 0
 
