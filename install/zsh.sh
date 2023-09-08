6e0e2408cbfc99e5d5c64b50d8479a604af0bbb4
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-08 23:16:33 +0100

    Add oh my zsh

--- install/zsh.sh
+++ install/zsh.sh
@@ -2,3 +2,7 @@
 
 sudo apt install zsh -y
 sudo chsh -s $(which zsh) $(whoami)
+
+rm -rf ~/.oh-my-zsh 2>/dev/null
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
