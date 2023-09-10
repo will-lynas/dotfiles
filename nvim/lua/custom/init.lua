17fe21dee05715662330ccb4d3827a160939e127
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-10 13:32:11 +0100

    Change local leader

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+vim.g.maplocalleader = "\\"
 require("lazy").setup("custom.plugins")
 
 require("custom.remap")
