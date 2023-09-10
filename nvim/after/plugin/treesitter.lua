d5137271159ec5ab0e234c374e4d7eafa27f7d4d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-10 14:00:11 +0100

    Disable treesitter for latex

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -11,6 +11,7 @@ require'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
+        disable = { "latex" },
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
