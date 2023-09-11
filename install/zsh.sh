eba2353c5fd6f2388c8137002868b4bdd7c4906b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-11 14:26:49 +0100

    Add you should use

--- install/zsh.sh
+++ install/zsh.sh
@@ -6,3 +6,7 @@ sudo chsh -s $(which zsh) $(whoami)
 rm -rf ~/.oh-my-zsh 2>/dev/null
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
+
+rm -rf ~/.my_zsh_plugins
+mkdir ~/.my_zsh_plugins
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.my_zsh_plugins/zsh-you-should-use
