8e5d4ee8ff2e38a51fd265edd81c3264e4241205
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-11 17:45:58 +0100

    Small changes

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+rm -rf ~/.my_zsh_plugins 2>/dev/null # Legacy
+
 sudo apt install zsh -y
 sudo chsh -s $(which zsh) $(whoami)
 
 rm -rf ~/.oh-my-zsh 2>/dev/null
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+
 git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
 
-rm -rf ~/.my_zsh_plugins 2>/dev/null # Legacy
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git /home/lynasw/.oh-my-zsh/custom/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
