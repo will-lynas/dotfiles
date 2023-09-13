d4a20a8f37a9615b3fea5119149b4854fd90fd8b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-13 23:45:19 +0100

    Add some git aliases

--- common_shell_scripts/aliases.sh
+++ common_shell_scripts/aliases.sh
@@ -13,3 +13,17 @@ alias ta="tmux a"
 alias zc="source ~/.zshrc" # Zsh Config
 alias l="ls -lABh"
 alias la="ls -la"
+
+# Custom git - modified from oh my zsh git plugin
+alias gl="git log"
+alias gst="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gc="git commit"
+alias gca="git commit -a"
+alias gcam="git commit -am"
+alias gf="git fetch"
+alias grh="git reset --hard"
+alias gco="git checkout"
+alias gp="git push"
+alias gaa="git add --all"
