80dbfe38118f066b4538f4a9cfb8b9164b3b1e1a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-13 23:32:41 +0100

    Use powerlevel10k

--- install/get_zsh.sh
+++ install/get_zsh.sh
@@ -5,3 +5,5 @@ sudo chsh -s $(which zsh) $(whoami)
 
 #git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
 #git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
+rm -rf ~/.powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
