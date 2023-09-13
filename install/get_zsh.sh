f274377e5679948cedf038ac8f054c4e58ca9258
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-13 23:41:20 +0100

    Add back zsh vi mode

--- install/get_zsh.sh
+++ install/get_zsh.sh
@@ -3,7 +3,9 @@
 sudo apt install zsh -y
 sudo chsh -s $(which zsh) $(whoami)
 
-#git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
 #git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
 rm -rf ~/.powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+
+rm -rf ~/.zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode.git ~/.zsh-vi-mode
