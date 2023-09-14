51042b7aa23c228a544bd3dfe2997fd2e5a67316
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-14 12:03:22 +0100

    Add tmux text macros

--- install/setup_tmux.sh
+++ install/setup_tmux.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+source ~/.df/install/backups.sh
+
 sudo apt install tmux -y
+
 backup_rm ~/.config/tmux
 backup_rm ~/.tmux.conf
 ln -s ~/.df/tmux ~/.config/tmux
-ln -s ~/.config/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.df/tmux/tmux.conf ~/.tmux.conf
+
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+rm -rf ~/.tmux/plugins/tmux-text-macros
+git clone https://github.com/Neo-Oli/tmux-text-macros ~/.tmux/plugins/tmux-text-macros
+backup_rm ~/.tmux/custom-macros
+ln -s ~/.df/tmux/custom-macros ~/.tmux/custom-macros
