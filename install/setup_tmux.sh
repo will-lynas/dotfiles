4cd86ebd211f758dd52a79a2af42b9ab8a6aff52
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-16 13:23:35 +0100

    Allow selection of current session

--- install/setup_tmux.sh
+++ install/setup_tmux.sh
@@ -12,5 +12,5 @@ ln -s ~/.df/tmux/tmux.conf ~/.tmux.conf
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-rm -rf ~/.tmux/plugins/tmux-text-macros
+rm -rf ~/.tmux_macros
 git clone https://github.com/will-lynas/tmux_macros ~/.tmux_macros
