338486f37e8b1404d52e63433d1522de9f0031ea
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-18 22:52:34 +0100

    Alias - git set credentials

--- common_shell_scripts/aliases.sh
+++ common_shell_scripts/aliases.sh
@@ -36,3 +36,6 @@ alias gcm="git commit -m"
 alias grb="git rebase"
 alias grbc="git rebase --continue"
 alias grba="git rebase --abort"
+
+# g[it] s[et] c[redentials]
+alias gsc="git config user.name 'Will Lynas' && git config user.email '43895423+will-lynas@users.noreply.github.com'"
