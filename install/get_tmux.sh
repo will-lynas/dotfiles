b39c35b1c9168cc0ae28819386efbc2266930982
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-20 01:48:37 +0100

    Remove apt prompt on install script

--- install/get_tmux.sh
+++ install/get_tmux.sh
@@ -3,7 +3,7 @@
 sudo apt install -y libevent-dev ncurses-dev build-essential bison pkg-config automake autotools-dev
 
 rm -rf ~/.tmux
-sudo apt remove tmux # legacy
+sudo apt remove tmux -y # legacy
 git clone https://github.com/tmux/tmux.git ~/.tmux
 cd ~/.tmux
 sh autogen.sh
