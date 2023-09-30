3e949c36e1ffda026e23c01de1027fb763192ea1
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-10-01 00:11:03 +0100

    Add more luasnip config

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -1,11 +1,31 @@
 vim.cmd[[
-" Use Tab to expand and jump through snippets
+" Expand or jump in insert mode
 imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+
+" Jump forward through tabstops in visual mode
 smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
-" Use Shift-Tab to jump backwards through snippets
+" Jump backward through snippet tabstops with Shift-Tab (for example)
 imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+
+" Cycle forward through choice nodes with Control-f (for example)
+imap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
+smap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
 ]]
 
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
+require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
+vim.keymap.set('n', '<Leader>ls', function() require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"}) end)
+
+
+require("luasnip").config.set_config({
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+
+  update_events = 'TextChanged,TextChangedI'
+})
+
