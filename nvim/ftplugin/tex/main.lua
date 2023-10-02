ea2103f425befd86ee954eed7bd9c0177d230853
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-10-02 16:20:50 +0100

    Add basic latex editing config

---
+++ nvim/ftplugin/tex/main.lua
@@ -0,0 +1,5 @@
+vim.cmd[[
+set conceallevel=0
+let g:vimtex_quickfix_open_on_warning = 0
+let g:vimtex_view_method = 'zathura'
+]]
--- nvim/lua/luasnip-helper-funcs.lua
+++ nvim/lua/luasnip-helper-funcs.lua
@@ -1,17 +1,27 @@
--- ~/.config/nvim/lua/luasnip-helper-funcs.lua
 local M = {}
 
--- Be sure to explicitly define these LuaSnip node abbreviations!
-local ls = require("luasnip")
-local sn = ls.snippet_node
-local i = ls.insert_node
+M.ls = require("luasnip")
+M.s = M.ls.snippet
+M.sn = M.ls.snippet_node
+M.t = M.ls.text_node
+M.i = M.ls.insert_node
+M.f = M.ls.function_node
+M.d = M.ls.dynamic_node
+M.fmt = require("luasnip.extras.fmt").fmt
+M.fmta = require("luasnip.extras.fmt").fmta
+M.rep = require("luasnip.extras").rep
 
 function M.get_visual(args, parent)
   if (#parent.snippet.env.LS_SELECT_RAW > 0) then
-    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+    return M.sn(nil, M.i(1, parent.snippet.env.LS_SELECT_RAW))
   else
-    return sn(nil, i(1, ''))
+    return M.sn(nil, M.i(1, ''))
   end
 end
+
+function M.in_mathzone()
+    return vim.fn['vimtex#syntax#in_mathzone']() == 1
+end
+
 
 return M
