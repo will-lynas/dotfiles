1704b27db47080aca5f04102ca5294784d75a949
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-10-10 20:33:20 +0100

    Reformat aliases file

--- common_shell_scripts/aliases.sh
+++ common_shell_scripts/aliases.sh
@@ -6,33 +6,43 @@ alias egrep='egrep --color=auto'
 
 # Custom
 alias vim=nvim
-alias cat="batcat -n"
-alias catp="batcat -p"
+alias bat="batcat -n"
+alias batp="batcat -p"
 alias clear="clear -x"
 alias c="clear"
 alias e="exit"
-alias zc="source ~/.zshrc" # Zsh Config
+alias zc="source ~/.zshrc"
 alias zx="exec zsh"
 alias l="ls -lABh"
 alias la="ls -la"
-alias lg="git ls-files" # list git?
+alias lg="git ls-files" # List Git
 alias md="mkdir -p"
+alias sv="source venv/bin/activate" # Source Venv
+alias br="sudo brightnessctl set"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
 
 # Tmux
 alias ta="tmux a"
 alias ts="tmux new -s"
 
-# Custom git - modified from oh my zsh git plugin
-alias gp="git push"
-alias gpf="git push -f"
+# Git
 alias gf="git fetch"
 alias gl="git log"
 alias gst="git status"
 alias grh="git reset --hard"
+
+alias ga="git add"
+alias gaa="git add --all"
+
+alias gp="git push"
+alias gpf="git push -f"
+
 alias gco="git checkout"
 alias gcob="git checkout -b"
-alias gaa="git add --all"
-alias ga="git add"
 
 alias gd="git diff"
 alias gds="git diff --staged"
@@ -46,18 +56,8 @@ alias grb="git rebase"
 alias grbc="git rebase --continue"
 alias grba="git rebase --abort"
 
-# g[it] s[et] c[redentials]
-alias gsc="git config user.name 'Will Lynas' && git config user.email '43895423+will-lynas@users.noreply.github.com'"
+alias gsc="git config user.name 'Will Lynas' && git config user.email '43895423+will-lynas@users.noreply.github.com'" # Git Set Credentials
 
-# s[ource] v[env]
-alias sv="source venv/bin/activate"
-
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-
-alias br="sudo brightnessctl set"
-
+# Globals
 alias -g L="| less"
 alias -g ner="2> /dev/null"
