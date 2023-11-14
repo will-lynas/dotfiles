74f281869c4a329590290ffaa40b6ca2e7dc363b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-11-14 17:00:47 +0000

    Add python shebang snippet

---
+++ nvim/LuaSnip/python/all.lua
@@ -0,0 +1,16 @@
+local h = require('luasnip-helper-funcs')
+
+return {
+    h.s(
+    {
+        trig=";vp",
+        snippetType = "autosnippet"
+    },
+    h.fmta([[
+    #!/usr/bin/env python3.11
+    ]],
+    {}
+    ),
+    {}
+    ),
+}
