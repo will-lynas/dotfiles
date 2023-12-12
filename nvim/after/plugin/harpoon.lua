d2679aef4e0e0d379650692d7e09734ea75f2f4d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-12 23:18:47 +0000

    Unify leader keybindings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,8 +2,9 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>m", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>q", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>w", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>e", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>r", function() ui.nav_file(4) end)
