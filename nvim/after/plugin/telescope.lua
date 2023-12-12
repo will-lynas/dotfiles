2391ad19072667fd8a0caed50157b8f361f028bd
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-12 22:57:59 +0000

    Add more telescope builtin keybinds

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,13 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep> ")});
-end)
--- p[roject] b[uffers] ? IDK
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>pk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>pr', builtin.registers, {})
+vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>pt', builtin.vim_options, {})
+vim.keymap.set('n', '<leader>pm', builtin.man_pages, {})
+vim.keymap.set('n', '<leader>pd', function() builtin.diagnostics({bufnr=0}) end, {})
+vim.keymap.set('n', '<leader>pa', builtin.git_branches, {})
