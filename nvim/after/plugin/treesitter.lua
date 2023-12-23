ab779f771d0ae9f5325b763fedcd7f270c768d00
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-23 22:18:16 +0000

    Fix python indentation for brackets in strings

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -17,6 +17,6 @@ require'nvim-treesitter.configs'.setup {
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = { "python" }, -- Fix for python indentation
     },
 }
