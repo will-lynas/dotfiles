e54b82d95c49811e34a21141b8df9ade7d2cb91b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-27 01:43:57 +0000

    Remove .df symlink dependency

--- install/install.sh
+++ install/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-source ~/.df/install/backups.sh
+source ~/dotfiles/install/backups.sh
 
 mkdir -p ~/.config
 
 backup_rm ~/.config/nvim
-ln -s ~/.df/nvim ~/.config/nvim
+ln -s ~/dotfiles/nvim ~/.config/nvim
 
 backup_rm ~/.config/tmux
 backup_rm ~/.tmux.conf
-ln -s ~/.df/tmux ~/.config/tmux
-ln -s ~/.df/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 backup_rm ~/.config/Code/User/settings.json
-ln -s ~/.df/vscode/settings.json ~/.config/Code/User/settings.json
+ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 backup_rm ~/.config/Code/User/keybindings.json
-ln -s ~/.df/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
 touch ~/.zshrc 2>/dev/null
-LINE="source ~/.df/zsh/zshrc"
+LINE="source ~/dotfiles/zsh/zshrc"
 grep -q "$LINE" ~/.zshrc || echo "$LINE" >> ~/.zshrc
--- zsh/scripts/all.sh
+++ zsh/scripts/all.sh
@@ -1,3 +1,3 @@
-source ~/.df/zsh/scripts/aliases.sh
-source ~/.df/zsh/scripts/fzf_options.sh
-source ~/.df/zsh/scripts/fix_terminal_colors.sh
+source ~/dotfiles/zsh/scripts/aliases.sh
+source ~/dotfiles/zsh/scripts/fzf_options.sh
+source ~/dotfiles/zsh/scripts/fix_terminal_colors.sh
