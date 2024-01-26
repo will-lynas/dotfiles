f28fab2ee224593ffe99f2cab3d88f6b5aade400
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-01-26 20:31:05 +0000

    Add treesitter parsers required for noice.nvim

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "rust" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "rust",
+    "markdown", "markdown_inline", "regex" };
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
