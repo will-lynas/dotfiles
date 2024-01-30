94cc3db28af2fed1e88e3981295a3b925c8c2bea
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-01-30 12:44:28 +0000

    Make pf find hidden files but not anything in .git

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git'}}) end, {})
 vim.keymap.set('n', '<leader>pc', function() builtin.find_files({hidden=true, no_ignore=true, no_ignore_parent=true}) end, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
