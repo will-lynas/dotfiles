bf7ed33912851472732e363a056cf72b46b0f2f0
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-02-12 17:18:40 +0000

    Turn off mouse

--- nvim/lua/custom/set.lua
+++ nvim/lua/custom/set.lua
@@ -40,7 +40,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   command = [[%s/\s\+$//e]],
 })
 
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 vim.opt.list = false -- Add a toggle function for this later. Disabled for the moment.
 vim.opt.listchars = "leadmultispace:    ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽   ⸽,extends:▸,precedes:◂,"
