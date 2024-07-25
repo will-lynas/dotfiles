24a81e691033d435c78059f94adfe28c63a41c45
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-25 23:07:36 +0100

    feat: Add lazydev

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,7 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
+		"folke/lazydev.nvim",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-nvim-lsp",
 		"neovim/nvim-lspconfig",
@@ -21,6 +22,7 @@ return {
 			sources = {
 				{ name = "path" },
 				{ name = "nvim_lsp" },
+				{ name = "lazydev" },
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-n>"] = cmp.mapping.select_next_item(),
