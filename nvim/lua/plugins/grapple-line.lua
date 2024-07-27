a7cf829346435cc054e66aa44b0307fda29fb86c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-27 20:26:31 +0100

    Add dev options for grapple-line.nvim

--- nvim/lua/plugins/grapple-line.lua
+++ nvim/lua/plugins/grapple-line.lua
@@ -1,11 +1,24 @@
 return {
-	"will-lynas/grapple-line.nvim",
+	name = "grapple-line.nvim",
+	dir = "~/dev/grapple-line.nvim/",
 	dependencies = {
 		"cbochs/grapple.nvim",
 	},
-	version = "1.x",
 	opts = {
-		show_names = true,
-		overflow = "ellipsis",
+		number_of_files = 4,
+		colors = {
+			active = "lualine_a_normal",
+			inactive = "lualine_a_inactive",
+		},
+		-- Accepted values:
+		-- "unique_filename" shows the filename and parent directories if needed
+		-- "filename" shows the filename only
+		mode = "unique_filename",
+		-- If a tag name is set, use that instead of the filename
+		show_names = false,
+		-- Accepted values:
+		-- "none" - overflowing files are ignored
+		-- "ellipsis" - if there are overflowing files an ellipsis will be shown
+		overflow = "none",
 	},
 }
