dc3d5bdbc45c137523f45d7c8ec29b5d2eb73cd9
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-28 23:58:13 +0100

    Fix nvim-treesitter config
    This was completely broken

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		ensure_installed = { "all" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = { "python" }, -- Fix for python indentation
-		},
-	},
+	build = ":TSUpdate",
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = "all",
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "python" },
+			},
+		})
+	end,
 }
