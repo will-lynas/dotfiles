3eba0ce6b53d1d00a3dc9b475afe4d00814bea0d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-29 23:36:16 +0100

    Update which key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,24 +1,19 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-	end,
-	opts = {},
 	config = function()
-		require("which-key").register({
-			["<leader>p"] = { name = "Telescope", _ = "which_key_ignore" },
-			["<leader>x"] = { name = "Execute", _ = "which_key_ignore" },
-			["<leader>k"] = { name = "Keep Buffer", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "System Clipboard", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Hunk", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "Toggle", _ = "which_key_ignore" },
-			["<leader>l"] = { name = "LSP", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "Neotree", _ = "which_key_ignore" },
-			["<leader>i"] = { name = "Tab", _ = "which_key_ignore" },
-			["<leader>o"] = { name = "Grapple", _ = "which_key_ignore" },
-			["<leader>z"] = { name = "Lazy", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader>z", group = "Lazy" },
+			{ "<leader>p", group = "Telescope" },
+			{ "<leader>x", group = "Execute" },
+			{ "<leader>k", group = "Keep Buffer" },
+			{ "<leader>s", group = "System Clipboard" },
+			{ "<leader>h", group = "Hunk" },
+			{ "<leader>t", group = "Toggle" },
+			{ "<leader>l", group = "LSP" },
+			{ "<leader>d", group = "Neotree" },
+			{ "<leader>i", group = "Tab" },
+			{ "<leader>o", group = "Grapple" },
 		})
 	end,
 }
