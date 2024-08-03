9f2723eb46dfc42fcbfdf7c653544419c9b263a5
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-03 14:45:39 +0100

    Add dev keybinds for grapple-utils

--- nvim/lua/plugins/grapple-utils.lua
+++ nvim/lua/plugins/grapple-utils.lua
@@ -1,10 +1,21 @@
+local utils = require("utils")
+
 return {
 	"will-lynas/grapple-utils.nvim",
 	dependencies = {
 		"cbochs/grapple.nvim",
 	},
 	config = function()
-		local utils = require("grapple-utils")
-		vim.keymap.set("n", "<leader>oe", utils.move_to_end, { desc = "Move To End" })
+		local grapple_utils = require("grapple-utils")
+
+		vim.keymap.set("n", "<leader>oe", grapple_utils.move_to_end, { desc = "Move To End" })
+
+		if grapple_utils["move_up"] then
+			vim.keymap.set("n", "<leader>oh", grapple_utils.move_up, { desc = "Move Left" })
+		end
+
+		if grapple_utils["move_down"] then
+			vim.keymap.set("n", "<leader>ol", grapple_utils.move_down, { desc = "Move Right" })
+		end
 	end,
 }
