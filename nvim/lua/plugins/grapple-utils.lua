f45ad7be9c211d8a9b511fd7ec3c54d8f47a671f
Artem Streltsov <artem@streltsov.dev>
2024-08-03 17:29:11 +0500

    add keybinds for new grapple-utils functions

--- nvim/lua/plugins/grapple-utils.lua
+++ nvim/lua/plugins/grapple-utils.lua
@@ -6,5 +6,7 @@ return {
 	config = function()
 		local utils = require("grapple-utils")
 		vim.keymap.set("n", "<leader>oe", utils.move_to_end, { desc = "Move To End" })
+		vim.keymap.set("n", "<leader>ou", utils.move_up, { desc = "Move One Up" })
+		vim.keymap.set("n", "<leader>od", utils.move_down, { desc = "Move One Down" })
 	end,
 }
