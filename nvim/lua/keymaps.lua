f83390b0200bf96e95dd09a89afb2b9a42cb4c4e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-03 20:50:17 +0100

    Add keybinds for inserting date and time

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -34,6 +34,21 @@ local function toggle_wrap_all()
 	end
 end
 
+local function insert_date()
+	local date_str = tostring(os.date("%Y-%m-%d"))
+	vim.api.nvim_put({ date_str }, "c", false, true)
+end
+
+local function insert_time()
+	local time_str = tostring(os.date("%H:%M:%S"))
+	vim.api.nvim_put({ time_str }, "c", false, true)
+end
+
+local function insert_date_time()
+	local date_time_str = tostring(os.date("%Y-%m-%d %H:%M:%S"))
+	vim.api.nvim_put({ date_time_str }, "c", false, true)
+end
+
 -- Select text that was last pasted
 vim.keymap.set("n", "gp", "`[v`]")
 
@@ -113,3 +128,7 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diagnostic float"
 
 vim.keymap.set("n", "<leader>zo", require("lazy").home, { desc = "Open" })
 vim.keymap.set("n", "<leader>zr", require("lazy").restore, { desc = "Restore" })
+
+vim.keymap.set("n", "<leader>ad", insert_date, { desc = "Date" })
+vim.keymap.set("n", "<leader>at", insert_time, { desc = "Time" })
+vim.keymap.set("n", "<leader>al", insert_date_time, { desc = "Date and Time" })
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -14,6 +14,7 @@ return {
 			{ "<leader>d", group = "Neotree" },
 			{ "<leader>i", group = "Tab" },
 			{ "<leader>o", group = "Grapple" },
+			{ "<leader>a", group = "Insert" },
 		})
 	end,
 }
