7e28364f724743a1466f6b0a2fcb8750fc11e78c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-04 10:23:54 +0100

    Revert the last few changes

--- nvim/lua/plugins/grapple.lua
+++ nvim/lua/plugins/grapple.lua
@@ -13,29 +13,25 @@ return {
 			vim.cmd("Telescope grapple tags")
 		end, { desc = "Grapple" })
 
-		vim.keymap.set("n", "<leader>ot", grapple.toggle, { desc = "Toggle Tag" })
+		vim.keymap.set("n", "<leader>oa", grapple.toggle, { desc = "Toggle Tag" })
 		vim.keymap.set("n", "<leader>om", grapple.toggle_tags, { desc = "Tag Menu" })
-		vim.keymap.set("n", "<leader>oc", grapple.toggle_scopes, { desc = "Scopes Menu" })
+		vim.keymap.set("n", "<leader>os", grapple.toggle_scopes, { desc = "Scopes Menu" })
 		vim.keymap.set("n", "<leader>or", grapple.reset, { desc = "Reset" })
 
-		vim.keymap.set("n", "<leader>oa", function()
+		vim.keymap.set("n", "<M-q>", function()
 			require("grapple").select({ index = 1 })
 		end, { desc = "1", silent = true })
 
-		vim.keymap.set("n", "<leader>os", function()
+		vim.keymap.set("n", "<M-w>", function()
 			require("grapple").select({ index = 2 })
 		end, { desc = "2", silent = true })
 
-		vim.keymap.set("n", "<leader>od", function()
+		vim.keymap.set("n", "<M-e>", function()
 			require("grapple").select({ index = 3 })
 		end, { desc = "3", silent = true })
 
-		vim.keymap.set("n", "<leader>of", function()
+		vim.keymap.set("n", "<M-r>", function()
 			require("grapple").select({ index = 4 })
-		end, { desc = "4", silent = true })
-
-		vim.keymap.set("n", "<leader>og", function()
-			require("grapple").select({ index = 5 })
 		end, { desc = "4", silent = true })
 	end,
 }
