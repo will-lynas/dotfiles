9a4e763584d24f96725f1eae2ae846a3c4aa1762
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-23 13:54:19 +0200

    Shown hidden in Oil by default

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>jo", require("oil").open, { desc = "Open" })
 		vim.keymap.set("n", "<leader>jk", require("oil").close, { desc = "Close" })
