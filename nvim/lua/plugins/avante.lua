2fc5f9c2274f18e4ee1b71a0ed589882c7d7b25d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-31 12:49:20 +0100

    Update avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,7 +1,36 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
-	build = "make",
+	lazy = false,
+	build = ":AvanteBuild",
+	dependencies = {
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"zbirenbaum/copilot.lua",
+		{
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					use_absolute_path = true,
+				},
+			},
+		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
 	opts = {
 		mappings = {
 			ask = "<leader>;a",
@@ -11,19 +40,6 @@ return {
 				debug = "<leader>;d",
 				hint = "<leader>;h",
 			},
-		},
-	},
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		{
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
 		},
 	},
 }
