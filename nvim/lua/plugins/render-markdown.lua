42ace77d03c91310ee69635e5d361e0a9d74247a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-09-02 20:50:56 +0100

    Configure render-markdown

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,29 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
+	-- TODO: make this work on cmp docs
+	ft = { "markdown", "Avante" },
 	opts = {
 		-- TODO: do something about this circular dep
 		file_types = { "markdown", "Avante" },
+		-- TODO: highlighting is messed up for headings, so this is disabled for the moment
+		heading = {
+			enabled = false,
+		},
+		code = {
+			sign = false,
+			style = "normal",
+			border = "thick",
+		},
+		bullet = {
+			right_pad = 1,
+		},
+		link = {
+			image = "",
+			email = "",
+			hyperlink = "",
+		},
+		pipe_table = {
+			style = "normal",
+		},
 	},
-	ft = { "markdown", "Avante" },
 }
