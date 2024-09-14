3ce68c7e5e2fbecfbe8b0ee3660529e4ab0e9a41
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-09-15 00:56:09 +0200

    Remove the = thing in conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -13,18 +13,9 @@ return {
 		local conform = require("conform")
 		conform.setup({
 			formatters_by_ft = formatters,
-		})
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				local win_view = vim.fn.winsaveview()
-				local cursor_pos = vim.fn.getcurpos()
-				vim.cmd("normal! gg=G")
-				conform.format({ bufnr = args.buf })
-				vim.fn.setpos(".", cursor_pos)
-				vim.fn.winrestview(win_view)
-			end,
+			-- FIXME:
+			---@diagnostic disable-next-line
+			format_on_save = true,
 		})
 
 		vim.keymap.set("n", "<leader>fc", function()
