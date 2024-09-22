f5a3ad59da7697b3328b94dc0bd85873f7c1ce32
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-09-22 11:42:06 +0100

    Oil turn on skip confirm for simple

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			skip_confirm_for_simple_edits = true,
 			view_options = {
 				show_hidden = true,
 			},
