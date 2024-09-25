97fd28672a47c004b0e2ea00cab80965551f5233
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-09-25 02:03:37 +0100

    Local scope latex conceal level

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -6,9 +6,18 @@ return {
 		vim.cmd([[
             filetype plugin indent on
             syntax enable
-            set conceallevel=0
             let g:vimtex_view_method = 'zathura'
             let g:vimtex_quickfix_open_on_warning = 0
             ]])
+
+		local augroup = vim.api.nvim_create_augroup("latexSettings", { clear = true })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = augroup,
+			pattern = "latex",
+			callback = function()
+				vim.opt_local.conceallevel = 0
+			end,
+		})
 	end,
 }
