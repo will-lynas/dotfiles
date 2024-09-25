e0209941474f7fc29ab4e18cdc3a3c7295e68e29
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-09-25 02:07:17 +0100

    Remove vimscript from vimtex config

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,14 +2,8 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	config = function()
-		-- TODO: refactor to lua
-		-- TODO: local scope these settings
-		vim.cmd([[
-            filetype plugin indent on
-            syntax enable
-            let g:vimtex_view_method = 'zathura'
-            let g:vimtex_quickfix_open_on_warning = 0
-            ]])
+		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_quickfix_open_on_warning = 0
 
 		local augroup = vim.api.nvim_create_augroup("latexSettings", { clear = true })
 
