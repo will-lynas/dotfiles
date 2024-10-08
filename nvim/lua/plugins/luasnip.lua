ac7fde66f2acbaeed530af4b799c65c7a8683837
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-08 01:04:14 +0100

    Add constructor snippet for lean

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -6,5 +6,18 @@ return {
 	},
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
+
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+
+		ls.add_snippets("lean", {
+			s("cs", {
+				t({ "constructor", "· " }),
+				i(0),
+				t({ "", "  sorry", "·", "  sorry" }),
+			}),
+		})
 	end,
 }
