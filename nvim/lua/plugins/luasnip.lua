8e7d4b53aaabfc487964954b6a3365412ba0381a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-08 01:30:40 +0100

    Add lean induction snippet

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -11,6 +11,7 @@ return {
 		local s = ls.snippet
 		local t = ls.text_node
 		local i = ls.insert_node
+		local r = require("luasnip.extras").rep
 
 		ls.add_snippets("lean", {
 			s("cs", {
@@ -28,6 +29,14 @@ return {
 			s("ot", {
 				t("obtain ⟨ _, _ ⟩ := "),
 				i(1),
+			}),
+
+			s("nd", {
+				t("induction "),
+				i(1),
+				t({ " with", "| zero =>", "  sorry", "| succ " }),
+				r(1),
+				t({ " ih =>", "  sorry" }),
 			}),
 		})
 	end,
