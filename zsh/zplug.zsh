825b2eb1d432ed7081cad65b5b0fffc637c83987
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-29 15:08:55 +0000

    move stuff into main zshrc

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,28 +1,3 @@
-export PLUGIN_HOME="$HOME/.zsh_plugins"
-mkdir -p "$PLUGIN_HOME"
-
-if ! command -v git &> /dev/null; then
-    echo "git is not installed. Exiting plugin script..."
-    return 1
-fi
-
-export P10K_HOME="$PLUGIN_HOME/powerlevel10k"
-
-if [[ ! -d "$P10K_HOME" ]]; then
-    echo "powerlevel10k is not installed. Cloning..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K_HOME
-fi
-
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-source $P10K_HOME/powerlevel10k.zsh-theme
-# If this isn't symlinked, the config wizard will start
-source ~/.p10k.zsh
-
-# ----------------------------------------------------------------------------
-
 export ZPLUG_HOME="$HOME/.zplug"
 
 if ! command -v git &> /dev/null; then
@@ -37,7 +12,6 @@ fi
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "jeffreytse/zsh-vi-mode"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "plugins/git", from:oh-my-zsh
 
@@ -49,10 +23,3 @@ if ! zplug check --verbose; then
 fi
 
 zplug load
-
-if zplug check jeffreytse/zsh-vi-mode; then
-    export ZVM_VI_EDITOR="nvim"
-    export ZVM_VI_ESCAPE_BINDKEY="^["
-    export ZVM_VI_INSERT_ESCAPE_BINDKEY=$ZVM_VI_ESCAPE_BINDKEY
-    export ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
-fi
