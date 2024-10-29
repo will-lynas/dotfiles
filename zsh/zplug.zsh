6b88b7f1f1a839ff8feae7902955b8e661ecddc4
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-29 14:52:19 +0000

    move p10k out of zplug

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,3 +1,27 @@
+export PLUGIN_HOME="$HOME/.zsh_plugins"
+mkdir -p "$PLUGIN_HOME"
+
+if ! command -v git &> /dev/null; then
+    echo "git is not installed. Exiting plugin script..."
+    return 1
+fi
+
+export P10K_HOME="$PLUGIN_HOME/powerlevel10k"
+
+if [[ ! -d "$P10K_HOME" ]]; then
+    echo "powerlevel10k is not installed. Cloning..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K_HOME
+fi
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+source $P10K_HOME/powerlevel10k.zsh-theme
+source ~/.p10k.zsh
+
+# ----------------------------------------------------------------------------
+
 export ZPLUG_HOME="$HOME/.zplug"
 
 if ! command -v git &> /dev/null; then
@@ -15,7 +39,6 @@ source $ZPLUG_HOME/init.zsh
 zplug "jeffreytse/zsh-vi-mode"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "plugins/git", from:oh-my-zsh
-zplug "romkatv/powerlevel10k", as:theme, depth:1
 
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
@@ -25,15 +48,6 @@ if ! zplug check --verbose; then
 fi
 
 zplug load
-
-if zplug check romkatv/powerlevel10k; then
-    if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-      source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-    fi
-
-    source $ZSH_DIR/p10k.zsh
-fi
-
 
 if zplug check jeffreytse/zsh-vi-mode; then
     export ZVM_VI_EDITOR="nvim"
