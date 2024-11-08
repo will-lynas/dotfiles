0df8fcd0af5d3fad9afb2fa836d0955b100beb77
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-11-08 20:34:14 +0000

    add dracula

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,12 +1,16 @@
+-- This is a mess
 return {
 	"folke/tokyonight.nvim",
+	dependencies = { "Mofiqul/dracula.nvim" },
 	lazy = false,
 	priority = 1000,
 	config = function()
 		require("tokyonight").setup({
 			style = "moon",
 		})
-		-- This is probably not the best place for this
+
+		require("dracula").setup()
+
 		require("current-theme")
 	end,
 }
