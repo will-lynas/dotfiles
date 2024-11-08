1aedbfda4a5b14b49aef7f01b92e7fbf52b553ab
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-11-08 20:39:04 +0000

    add kanagawa

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/tokyonight.nvim",
-	dependencies = { "Mofiqul/dracula.nvim" },
+	dependencies = { "Mofiqul/dracula.nvim", "rebelot/kanagawa.nvim" },
 	lazy = false,
 	priority = 1000,
 	config = function()
