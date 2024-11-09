9959fcfa803fbacfd9e146da5f7005179a7af885
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-11-09 18:45:24 +0000

    add trouble.nvim

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/trouble.nvim",
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>to",
+			"<cmd>Trouble diagnostics toggle focus=false win.position=right<cr>",
+			desc = "Trouble diagnostics",
+		},
+	},
+}
